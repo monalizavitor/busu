0b13407621f799fc42839c3749fb7addde6f16d7
monaliza-vitor <vitormonaliza@gmail.com>
2020-12-10 14:02:41 -0300

    add rota de index

--- app-busu/src/routes/index.js
+++ app-busu/src/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // GET home page
-router.get('/', (req, res) => 
-    res.status(200).send({ titulo: 'Bem vindas (os) ao Busu!', version: '0.01'})
-)
+router.get('/', (req, res) => {
+    res.status(200).send({ titulo: 'Bem vinda(o) ao Busu!', version: '0.01'})
+})
 
 module.exports = router
