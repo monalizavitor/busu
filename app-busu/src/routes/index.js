c4942e99eba91137de965354062f33ada23406f3
monaliza-vitor <vitormonaliza@gmail.com>
2020-12-10 01:57:21 -0300

    add readme

--- app-busu/src/routes/index.js
+++ app-busu/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // GET home page
 router.get('/', (req, res) => 
-    res.send({ home: 'Bem vindas (os) ao Busu! v0.01'})
+    res.status(200).send({ titulo: 'Bem vindas (os) ao Busu!', version: '0.01'})
 )
 
 module.exports = router
